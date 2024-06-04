b784c7dda2b29aa751543177f95e70bfbbd87993
surenthvignesh <surenthvigneshesv@gmail.com>
2024-06-04 22:48:11 +0530

    Making the Specs Pass

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,34 +1,44 @@
+# Making the Specs Pass
+class Card
+	attr_reader :type
+
+	def initialize(type)
+		@type = type
+	end
+end
+
 RSpec.describe 'Card' do
 	it 'has a type' do
 		# specify 'has a type' - Can use both it or specify. Both are identical
 		card = Card.new('Ace of Spades')
 		expect(card.type).to eq('Ace of Spades')
 		# expect(1+1).to(eq(2))
-
 	end
 end
 
 
 # Assignment
-RSpec.describe 'math_calculations' do
-	it 'does basic math' do
-		assert_1 = 5 + 5
-		assert_2 = 5 * 5
-		assert_3 = 5 - 5
-		assert_4 = 5 / 5
+# RSpec.describe 'math_calculations' do
+# 	it 'does basic math' do
+# 		assert_1 = 5 + 5
+# 		assert_2 = 5 * 5
+# 		assert_3 = 5 - 5
+# 		assert_4 = 5 / 5
 
-		expect(assert_1).to eq(10)
-		expect(assert_2).to eq(25)
-		expect(assert_3).to eq(0)
-		expect(assert_4).to eq(1)
+# 		expect(assert_1).to eq(10)
+# 		expect(assert_2).to eq(25)
+# 		expect(assert_3).to eq(0)
+# 		expect(assert_4).to eq(1)
 
-	end
-end
+# 	end
+# end
 
 
-Reading_Failures_Topic:
-	To run the rspec files,
-	cd~ rspec - this will run all the rspec files.
+# Reading_Failures_Topic:
+# 	To run the rspec files,
+# 	cd~ rspec - this will run all the rspec files.
 
-	To run specific files,
-	cd~ rspec spec/card_spec.rb
+# 	To run specific files,
+# 	cd~ rspec spec/card_spec.rb
+
+
