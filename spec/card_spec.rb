d524a8bae1c286fa5dc41790001a300cc9b0aebd
surenthvignesh <surenthvigneshesv@gmail.com>
2024-06-05 12:45:36 +0530

    Helper method added

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,20 +8,24 @@ class Card
 	end
 end
 
-# Before hooks and instance variables
+# Use Helper Methods
 RSpec.describe Card do	
 
-	before do
-	# before(:example) do
-		@card = Card.new('Ace', 'Spades')
+	# before do
+	# # before(:example) do
+	# 	@card = Card.new('Ace', 'Spades')
+	# end
+
+	def card
+		Card.new('Ace', "Spades")
 	end
 
 	it 'has a rank' do
-		expect(@card.rank).to eq('Ace')
+		expect(card.rank).to eq('Ace')
 	end
 
 	it 'has a suit' do
-		expect(@card.suit).to eq('Spades')
+		expect(card.suit).to eq('Spades')
 	end
 
 end
